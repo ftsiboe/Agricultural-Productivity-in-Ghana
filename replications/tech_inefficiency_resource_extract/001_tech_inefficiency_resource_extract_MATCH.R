6e39d116f3b75b3d850411044f9ee636fee2f10d
Francis Tsiboe <ftsiboe@warlock18.beocat.ksu.edu>
2025-03-23 03:35:19 -0500

    updates

---
+++ replications/tech_inefficiency_resource_extract/001_tech_inefficiency_resource_extract_MATCH.R
@@ -0,0 +1,134 @@
+rm(list=ls(all=TRUE));library('magrittr');library(future.apply);library(dplyr);library(purrr)
+library(MatchIt);library(randomForest);library(CBPS);library(dbarts);library(optmatch);library(Matching);library(rgenoud)
+setwd(ifelse(Sys.info()['sysname'] =="Windows",getwd(),"/homes/ftsiboe/Articles/GH/GH_CropProd_Resource_Extract/"))
+source("/homes/ftsiboe/Articles/GH/v001_GH_CropProd_FXN.R")
+REPO <- ifelse(Sys.info()['sysname'] =="Windows","C:/Research/Articles/Ghana/GH_CropProd_Resource_Extract/",
+               "/homes/ftsiboe/Articles/GH/GH_CropProd_Resource_Extract/")
+dir.create(paste0("Results"))
+dir.create(paste0("Results/matching/"))
+
+DATA <- Fxn_DATA_Prep(as.data.frame(haven::read_dta("Data/Harmonized_Farm_resources_extraction_Data.dta")))
+DATA <- DATA[as.character(haven::as_factor(DATA$CropID)) %in% "Pooled",]
+DATA$Treat <- DATA$extraction_any #%in% 1
+
+Arealist <- names(DATA)[grepl("Area_",names(DATA))]
+Arealist <- Arealist[Arealist%in% paste0("Area_",c("Beans","Cassava","Cocoa","Cocoyam","Maize","Millet","Okra","Palm","Peanut",
+                                                   "Pepper","Plantain","Rice","Sorghum","Tomatoe","Yam"))]
+
+Emch <- c("Survey","Region","Ecozon","Locality","Female")
+Scle <- c("AgeYr","YerEdu","HHSizeAE","FmleAERt","Depend","CrpMix",Arealist)
+Fixd <- c("Credit","OwnLnd","Ethnic","Marital","Religion","Head")
+
+Emch.formula  <- paste0(paste0("factor(",Emch,")"),collapse = "+")
+Match.formula <- paste0("Treat~",paste0(c(Scle),collapse = "+"))
+for(var in c(Fixd)){ Match.formula<-paste0(Match.formula,"+factor(",var,")")}
+
+DATA <- DATA[complete.cases(DATA[c("Surveyx","EaId","HhId","Mid","UID","Weight","Treat",Emch,Scle,Fixd)]),]
+summary(DATA[c(Emch,Scle,Fixd)])
+
+function(){
+  m.specs <- Fxn_draw_spec(drawN=100,DATA=DATA,myseed=03182025)
+  saveRDS(m.specs$m.specs,file="Results/mspecs.rds")
+  saveRDS(m.specs$drawlist,file="drawlist.rds")
+}
+
+m.specs <- readRDS("Results/mspecs.rds")
+
+# m.specs <- m.specs[! paste0("Results/matching/Match",stringr::str_pad(m.specs$ARRAY,4,pad="0"),".rds") %in% 
+#                      list.files(paste0("Results/matching"),full.names = T),]
+
+if(!is.na(as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")))){
+  m.specs <- m.specs[as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")),]
+}
+
+lapply(
+  1:nrow(m.specs), #
+  function(i,DATA){
+    tryCatch({
+      # i <- 10;m.data <- DATA
+      Sampels <- Fxn_Sampels(DATA=DATA,Emch=Emch,Scle=Scle,Fixd=Fixd,m.specs=m.specs,i=i)
+      if(! m.specs$boot[i] %in% 0){Sampels[["m.out"]] <- NULL}
+      saveRDS(Sampels,file=paste0("Results/matching/Match",stringr::str_pad(m.specs$ARRAY[i],4,pad="0"),".rds"))
+    }, error=function(e){})
+    return(i)
+  },DATA=DATA)
+
+function(){
+  Fxn_Covariate_balance()
+  
+  mspecs_optimal <- readRDS(paste0("Results/mspecs_optimal.rds"))[c("ARRAY","method","distance","link")]
+  mspecs_fullset <- readRDS(paste0("Results/mspecs.rds"))
+  mspecs_fullset <- mspecs_fullset[!grepl("linear",mspecs_fullset$link),]
+  mspecs_fullset <- mspecs_fullset[mspecs_fullset$boot %in% 0,c("ARRAY","method","distance","link")]
+  
+  m.specs <- readRDS(paste0("Results/mspecs.rds"))
+  m.specs <- m.specs[m.specs$boot %in% 0,]
+  m.specs <- m.specs[m.specs$method %in% mspecs_fullset$method,]
+  m.specs <- m.specs[m.specs$distance %in% mspecs_fullset$distance,]
+  m.specs <- m.specs[m.specs$link %in% mspecs_fullset$link,]
+  m.specs$name <- ifelse(m.specs$link %in% NA,m.specs$distance,m.specs$link)
+  
+  atet <- as.data.frame(
+    data.table::rbindlist(
+      lapply(
+        1:nrow(m.specs),
+        function(mm){
+          # mm <- 1
+          DONE <- NULL
+          tryCatch({
+            md <- dplyr::inner_join(unique(readRDS(paste0("Results/matching/Match",stringr::str_pad(m.specs$ARRAY[mm],4,pad="0"),".rds"))$md),
+                                    DATA,by=c("Surveyx","EaId","HhId","Mid","UID"))
+            
+            md$HrvstKg <- md$HrvstKg/md$Area
+            md$SeedKg <- md$SeedKg/md$Area
+            md$HHLaborAE <- md$HHLaborAE/md$Area
+            md$HirdHr <- md$HirdHr/md$Area
+            md$FertKg <- md$FertKg/md$Area
+            md$PestLt <- md$PestLt/md$Area
+            
+            atet_scalar <- as.data.frame(
+              data.table::rbindlist(
+                future_lapply(
+                  c("HrvstKg","Area", "SeedKg", "HHLaborAE","HirdHr","FertKg","PestLt"),
+                  function(outcome){
+                    # outcome <- "Area"
+                    DONE <- NULL
+                    tryCatch({
+                      atet_scalar <- marginaleffects::avg_comparisons(
+                        lm(as.formula(paste0("log(",outcome,"+0.00001)","~Treat*(",paste0(c(paste0("factor(",c(Emch,Fixd),")"),Scle),collapse = "+"),")")),
+                           data = md[!log(md[,outcome]+0.00001) %in% c(NA,NaN,Inf,-Inf),], weights = weights), variables = "Treat",
+                        newdata = subset(md[!log(md[,outcome]+0.00001) %in% c(NA,NaN,Inf,-Inf),], Treat == 1),wts = "weights",vcov="HC0")
+                      atet_scalar <- data.frame(outcome=outcome,as.data.frame(atet_scalar)[c("estimate","std.error","statistic","p.value","s.value","conf.low","conf.high")])
+                      DONE <- atet_scalar
+                    }, error=function(e){})
+                    return(DONE)
+                  }), fill = TRUE))
+            
+            atet_factor <- as.data.frame(
+              data.table::rbindlist(
+                future_lapply(
+                  c("Credit","Eqip","Extension","OwnLnd"),
+                  function(outcome){
+                    # outcome <- "Credit"
+                    DONE <- NULL
+                    tryCatch({
+                      atet_factor <- marginaleffects::avg_comparisons(
+                        glm(as.formula(paste0(outcome,"~Treat*(",paste0(c(paste0("factor(",c(Emch,Fixd),")"),Scle),collapse = "+"),")")),
+                            data = md, weights = weights,family = quasibinomial()), variables = "Treat",
+                        newdata = subset(md, Treat == 1),wts = "weights",vcov="HC0",comparison = "lnratioavg",transform = "exp")
+                      atet_factor <- data.frame(outcome=outcome,as.data.frame(atet_factor)[c("estimate","p.value","s.value","conf.low","conf.high")])
+                      atet_factor$std.error <- atet_factor$statistic <- NA
+                      DONE <- atet_factor
+                    }, error=function(e){})
+                    return(DONE)
+                  }), fill = TRUE))
+            
+            DONE <- data.frame(m.specs[mm,c("method","distance","name")],rbind(atet_factor,atet_scalar))
+          }, error=function(e){})
+          return(DONE)
+        }), fill = TRUE))
+  
+  saveRDS(atet,file=paste0("Results/matching_treatment_effects.rds"))
+}
+
+# unlink(list.files(getwd(),pattern =paste0(".out"),full.names = T))
---
+++ replications/tech_inefficiency_resource_extract/002_tech_inefficiency_resource_extract_MSF.R
@@ -0,0 +1,150 @@
+
+rm(list=ls(all=TRUE))
+setwd(ifelse(Sys.info()['sysname'] =="Windows",getwd(),"/homes/ftsiboe/Articles/GH/AgriculturalProductivityinGhana/"))
+PROJECT <- getwd()
+source(paste0(getwd(),"/codes/helpers_tech_inefficiency.R"))
+DATA <- Fxn_DATA_Prep(as.data.frame(haven::read_dta(paste0(getwd(),"/datasets/harmonized_crop_farmer_level_data.dta"))))
+
+setwd(paste0(getwd(),"/replications/tech_inefficiency_resource_extract"))
+dir.create("results")
+dir.create("results/estimations")
+
+FXNFORMS  <- Fxn_SF_forms()$FXNFORMS
+DISTFORMS <- Fxn_SF_forms()$DISTFORMS
+
+function(){
+  mainD <- 1
+  mainF <- 2
+  
+  SPECS <- Fxn_SPECS(TechVarlist=c("extraction_any","mining_any","mining_comm","mining_gala","quarrying","sand","salt"),  
+                     mainD = mainD, mainF=mainF)
+  SPECS <- SPECS[!SPECS$disasg %in% c("EduCat"),]
+  SPECS <- rbind(
+    data.frame(SPECS[ (SPECS$f %in% mainF & SPECS$d %in% mainD & SPECS$TechVar %in% "extraction_any" & SPECS$level %in% "Pooled"),], nnm="fullset"),
+    data.frame(SPECS[ (SPECS$f %in% mainF & SPECS$d %in% mainD & SPECS$TechVar %in% "extraction_any" & SPECS$level %in% "Pooled"),], nnm="optimal"),
+    data.frame(SPECS[!(SPECS$f %in% mainF & SPECS$d %in% mainD & SPECS$TechVar %in% "extraction_any" & SPECS$level %in% "Pooled"),], nnm="optimal"))
+  
+  SPECS <- SPECS[!(paste0(SPECS$disasg,"_",SPECS$level,"_",SPECS$TechVar,"_",names(FXNFORMS)[SPECS$f],"_",
+                          names(DISTFORMS)[SPECS$d],"_",SPECS$nnm,".rds") %in% list.files("Results/Estimations/")),]
+  
+  row.names(SPECS) <- 1:nrow(SPECS)
+  
+  saveRDS(SPECS,file="SPECS.rds")
+}
+
+SPECS <- readRDS("SPECS.rds")
+
+if(!is.na(as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")))){
+  SPECS <- SPECS[as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")),]
+}
+
+lapply(
+  c(1:nrow(SPECS)),
+  function(fit){
+    # fit <- 1
+    f <- SPECS$f[fit]
+    d <- SPECS$d[fit]
+    disasg <- SPECS$disasg[fit]
+    level <- SPECS$level[fit]
+    TechVar <- SPECS$TechVar[fit]
+    nnm <- SPECS$nnm[fit]
+    # nnm <- "optimal"
+    if(!paste0(disasg,"_",level,"_",TechVar,"_",names(FXNFORMS)[f],"_",names(DISTFORMS)[d],"_",nnm,".rds") %in% list.files("Results/Estimations/")){
+      #tryCatch({ 
+      
+      # Data Preparation
+      data <- DATA[DATA[,SPECS$disasg[fit]] %in% SPECS$level[fit],]
+      data$Tech <- as.numeric(as.integer(as.factor(as.character(data[,SPECS$TechVar[fit]]))))
+      if(!SPECS$disasg[fit] %in% "CropID") data <- data[data[,"CropID"] %in% "Pooled",]
+      TechKey   <- unique(data[c("Tech",SPECS$TechVar[fit])])
+      TechKey   <- TechKey[order(TechKey$Tech),]
+      
+      for(crop in c(c("Beans","Cassava","Cocoa","Cocoyam","Other","Millet","Okra","Palm","Peanut",
+                      "Pepper","Plantain","Rice","Sorghum","Tomatoe","Yam","Maize"))){
+        data[,paste0("CROP_",crop)] <- ifelse(data[,paste0("Area_",crop)] > 0, crop,NA)
+      }
+      
+      ArealistX <- names(data)[grepl("Area_",names(data))]
+      ArealistX <- ArealistX[ArealistX %in% paste0("Area_",c("Beans","Cassava","Cocoa","Cocoyam","Other","Millet","Okra","Palm","Peanut",
+                                                             "Pepper","Plantain","Rice","Sorghum","Tomatoe","Yam","Other"))]
+      
+      ArealistX <- apply(data[names(data)[names(data) %in% ArealistX]],2,mean) > 0.03
+      ArealistX <- names(ArealistX)[ArealistX %in% TRUE]
+      if(length(ArealistX)>0){ 
+        data$Area_Other <- 1 - rowSums(data[c(ArealistX[!ArealistX %in% "Area_Other"],"Area_Maize")],na.rm=T)
+        ArealistX <- unique(c(ArealistX,"Area_Other"))
+      }
+      
+      # draw estimations
+      drawlist = readRDS("drawlist.rds")
+      if(nnm %in% "fullset") drawlist <- drawlist[drawlist$ID<=50,]
+      res <- lapply(
+        unique(drawlist$ID),Fxn_draw_estimations,
+        data = data,
+        surveyy  = "Pooled" %in% data[,"CropID"],
+        intercept_shifters  = list(Svarlist=ArealistX,Fvarlist=c("Ecozon")),
+        intercept_shiftersM = list(Svarlist=ArealistX,Fvarlist=c("Ecozon")),
+        drawlist = drawlist,
+        wvar = "Weight",
+        yvar = "HrvstKg",
+        xlist = c("Area", "SeedKg", "HHLaborAE","HirdHr","FertKg","PestLt"),
+        ulist = list(Svarlist=c("lnAgeYr","lnYerEdu","CrpMix"),Fvarlist=c("Female","Ecozon","Extension","Credit","EqipMech","OwnLnd","Survey")),
+        ulistM= list(Svarlist=c("lnAgeYr","lnYerEdu","CrpMix"),Fvarlist=c("Female","Ecozon","Extension","Credit","EqipMech","OwnLnd","Survey")),
+        UID   = c("UID", "Survey", "CropID", "HhId", "EaId", "Mid"),
+        disagscors_list = c("Ecozon","Region","AgeCat","EduLevel","Female",names(data)[grepl("CROP_",names(data))]),
+        f     = f,
+        d     = d,
+        nnm=nnm,
+        tvar  = TechVar) 
+      
+      # draw summary [START FROM HERE]
+      res <- Fxn.draw_summary(res=res,TechKey=TechKey)
+      
+      for(xx in 1:length(res)){
+        tryCatch({
+          res[[xx]][,"FXN"]     <- names(FXNFORMS)[f]
+          res[[xx]][,"DIS"]     <- names(DISTFORMS)[d]
+          res[[xx]][,"disasg"]  <- disasg
+          res[[xx]][,"level"]   <- level
+          res[[xx]][,"TCH"]     <- TechVar
+          res[[xx]][,"TCHLvel"] <- factor(res[[xx]][,"Tech"],levels = c(-999,TechKey$Tech,999),labels = c("National",TechKey[,2],"Meta"))
+        }, error=function(e){})
+      }
+      
+      function(){
+        Main <- res$ef_mean
+        Main <- Main[Main$Survey %in% "GLSS0",]
+        Main <- Main[!Main$sample %in% "unmatched",]
+        Main <- Main[Main$restrict %in% "Restricted",]
+        
+        Main <- Main[Main$stat %in% "wmean",]
+        Main <- Main[Main$CoefName %in% "efficiencyGap_pct",]
+        Main <- Main[Main$estType %in% "teBC",]
+        Main[Main$type %in% "TGR",c("sample","type","TCHLvel","Estimate")]
+        Main[Main$type %in% "TE",c("sample","type","TCHLvel","Estimate")]
+        Main[Main$type %in% "MTE",c("sample","type","TCHLvel","Estimate")]
+        
+        
+        
+        Main <- res$el_mean
+        Main <- Main[Main$Survey %in% "GLSS0",]
+        Main <- Main[!Main$sample %in% "unmatched",]
+        Main <- Main[Main$stat %in% "wmean",]
+        Main <- Main[Main$CoefName %in% "elasticity",]
+        Main <- Main[Main$restrict %in% "Restricted",]
+        Main <- Main[Main$sample %in% "cloglog",]
+        Main
+        
+        
+      }
+      
+      res[["names"]] <- paste0(disasg,"_",level,"_",TechVar,"_",names(FXNFORMS)[f],"_",names(DISTFORMS)[d],"_",nnm)
+      saveRDS(res,file=paste0("Results/Estimations/",disasg,"_",level,"_",TechVar,"_",names(FXNFORMS)[f],"_",names(DISTFORMS)[d],"_",nnm,".rds"))
+      
+      #}, error=function(e){})
+    }
+    return(fit)
+  })
+
+# unlink(list.files(getwd(),pattern =paste0(".out"),full.names = T))
+
