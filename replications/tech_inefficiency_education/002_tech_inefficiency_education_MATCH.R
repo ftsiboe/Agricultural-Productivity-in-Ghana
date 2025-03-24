c4b46cc8304f28ba058296e1af1caf61ddf7c235
Francis Tsiboe <ftsiboe@wizard31.beocat.ksu.edu>
2025-03-24 17:32:58 -0500

    updates

--- replications/tech_inefficiency_education/002_tech_inefficiency_education_MATCH.R
+++ replications/tech_inefficiency_education/002_tech_inefficiency_education_MATCH.R
@@ -1,14 +1,16 @@
-
-rm(list=ls(all=TRUE));library('magrittr');library(future.apply);library(dplyr);library(purrr)
+rm(list=ls(all=TRUE));gc();library('magrittr');library(future.apply);library(dplyr);library(purrr)
 library(MatchIt);library(randomForest);library(CBPS);library(dbarts);library(optmatch);library(Matching);library(rgenoud)
-setwd(ifelse(Sys.info()['sysname'] =="Windows",getwd(),"/homes/ftsiboe/Articles/GH/GH_CropProd_Education/"))
-source("/homes/ftsiboe/Articles/GH/v001_GH_CropProd_FXN.R")
-REPO <- ifelse(Sys.info()['sysname'] =="Windows","C:/Research/Articles/Ghana/GH_CropProd_Education/",
-               "/homes/ftsiboe/Articles/GH/GH_CropProd_Education/")
-dir.create(paste0(REPO,"Results"))
-dir.create(paste0(REPO,"Results/matching/"))
 
-DATA <-  Fxn_DATA_Prep(as.data.frame(haven::read_dta("Data/Harmonized_Farm_Education_Data.dta")))
+setwd(ifelse(Sys.info()['sysname'] =="Windows",getwd(),"/homes/ftsiboe/Articles/GH/GH_AgricProductivityLab/"))
+PROJECT <- getwd()
+source(paste0(getwd(),"/codes/helpers_tech_inefficiency.R"))
+
+setwd(paste0(getwd(),"/replications/tech_inefficiency_education"))
+
+dir.create("results")
+dir.create("results/matching")
+
+DATA <- Fxn_DATA_Prep(as.data.frame(haven::read_dta("data/tech_inefficiency_education_data.dta")))
 
 DATA <- DATA[as.character(haven::as_factor(DATA$CropID)) %in% "Pooled",]
 DATA$Treat <- DATA$educated
@@ -29,15 +31,15 @@ DATA <- DATA[complete.cases(DATA[c("Surveyx","EaId","HhId","Mid","UID","Weight",
 summary(DATA[c(Emch,Scle,Fixd)])
 
 function(){
-  m.specs <- Fxn_draw_spec(drawN=100,DATA=DATA,myseed=02142025)
-  saveRDS(m.specs$m.specs,file="Results/mspecs.rds")
-  saveRDS(m.specs$drawlist,file="drawlist.rds")
+  m.specs <- Fxn_draw_spec(drawN=100,DATA=DATA,myseed=03242025)
+  saveRDS(m.specs$m.specs,file="results/mspecs.rds")
+  saveRDS(m.specs$drawlist,file="results/drawlist.rds")
 }
 
-m.specs <- readRDS("Results/mspecs.rds")
+m.specs <- readRDS("results/mspecs.rds")
 
-# m.specs <- m.specs[! paste0(REPO,"Results/matching/Match",stringr::str_pad(m.specs$ARRAY,4,pad="0"),".rds") %in% 
-#                      list.files(paste0(REPO,"Results/matching"),full.names = T),]
+# m.specs <- m.specs[! paste0("results/matching/Match",stringr::str_pad(m.specs$ARRAY,4,pad="0"),".rds") %in% 
+#                      list.files(paste0("results/matching"),full.names = T),]
 
 if(!is.na(as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")))){
   m.specs <- m.specs[as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID")),]
@@ -50,7 +52,7 @@ lapply(
       # i <- 10;m.data <- DATA
       Sampels <- Fxn_Sampels(DATA=DATA,Emch=Emch,Scle=Scle,Fixd=Fixd,m.specs=m.specs,i=i)
       if(! m.specs$boot[i] %in% 0){Sampels[["m.out"]] <- NULL}
-      saveRDS(Sampels,file=paste0(REPO,"Results/matching/Match",stringr::str_pad(m.specs$ARRAY[i],4,pad="0"),".rds"))
+      saveRDS(Sampels,file=paste0("results/matching/Match",stringr::str_pad(m.specs$ARRAY[i],4,pad="0"),".rds"))
     }, error=function(e){})
     return(i)
   },DATA=DATA)
