1b7477f0345938095f06869f237855a11042a07b
Francis Tsiboe <ftsiboe@wizard24.beocat.ksu.edu>
2025-03-26 16:39:49 -0500

    updates

--- replications/tech_inefficiency_education/004_tech_inefficiency_education_MSF.R
+++ replications/tech_inefficiency_education/004_tech_inefficiency_education_MSF.R
@@ -148,6 +148,15 @@ lapply(
       }
       
       res[["names"]] <- paste0(disasg,"_",level,"_",TechVar,"_",names(FXNFORMS)[f],"_",names(DISTFORMS)[d],"_",nnm)
+      
+      if(nnm %in% "fullset"){
+        res$rk_dist <- NULL
+        res$rk_mean <- NULL
+        res$rk_samp <- NULL
+        res$el_samp <- NULL
+        res$ef_samp <- NULL 
+      }
+      
       saveRDS(res,file=paste0("results/estimations/",disasg,"_",level,"_",TechVar,"_",names(FXNFORMS)[f],"_",names(DISTFORMS)[d],"_",nnm,".rds"))
       
       #}, error=function(e){})
