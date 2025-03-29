8c143049eb1918cdcafde5202fb138d8a9ad5622
Francis Tsiboe <ftsiboe@wizard39.beocat.ksu.edu>
2025-03-29 01:52:48 -0500

    updates

--- replications/tech_inefficiency_disability/100_tech_inefficiency_disability_FIGTAB.R
+++ replications/tech_inefficiency_disability/100_tech_inefficiency_disability_FIGTAB.R
@@ -19,21 +19,69 @@ openxlsx::saveWorkbook(wb,"results/tech_inefficiency_disability_results.xlsx",ov
 
 # Fig - Heterogeneity          
 rm(list= ls()[!(ls() %in% c(Keep.List))])
-fig_heterogeneity00(res=readRDS("results/estimations/CropID_Pooled_educated_TL_hnormal_optimal.rds")$disagscors,
-                    y_title="Percentage Difference (Disabled less Nondisabled)\n")
+fig <- fig_heterogeneity00(res=readRDS("results/estimations/CropID_Pooled_disabled_TL_hnormal_optimal.rds")$disagscors,
+                    y_title="Percentage Difference (Disabled less non-Disabled)\n")
+fig[["genderAge"]] <- fig[["genderAge"]] + theme(axis.text.x = element_text(size = 5.5))
+ggsave("results/figures/heterogeneity_crop_region.png", fig[["crop_region"]],dpi = 600,width = 8, height = 5)
+ggsave("results/figures/heterogeneity_genderAge.png", fig[["genderAge"]],dpi = 600,width = 8, height = 5)
+
 
 # Fig - Robustness              
 rm(list= ls()[!(ls() %in% c(Keep.List))])
-fig_robustness("\nDifference (%) [Educated less Uneducated]")
+fig_robustness(y_title="\nDifference (%) [Disabled less non-Disabled]",
+               res_list = c("results/estimations/CropID_Pooled_disabled_CD_hnormal_optimal.rds",
+                            list.files("results/estimations/",pattern = "CropID_Pooled_disabled_TL_",full.names = T)))
+
 
 # Fig - Matching TE      
 rm(list= ls()[!(ls() %in% c(Keep.List))])
+fig_input_te(y_title="\nEducation gap (%)",tech_lable=c("Full sample", "Disabled sample", "non-Disabled sample"))
 
-fig_input_te(y_title="\nEducation gap (%)",tech_lable=c("Full sample", "Educated sample", "Uneducated sample"))
 
 # Fig - Covariate balance 
 rm(list= ls()[!(ls() %in% c(Keep.List))])
 fig_covariate_balance()
+
+
+# Fig - Distribution 
+dataFrq <- readRDS("results/estimations/CropID_Pooled_disabled_TL_hnormal_fullset.rds")
+dataFrq <- dataFrq$ef_dist
+dataFrq <- dataFrq[dataFrq$estType %in% "teBC",]
+dataFrq <- dataFrq[dataFrq$Survey %in% "GLSS0",]
+dataFrq <- dataFrq[dataFrq$stat %in% "weight",]
+dataFrq <- dataFrq[dataFrq$restrict %in% "Restricted",]
+dataFrq$Tech <- factor(as.numeric(as.character(dataFrq$TCHLvel)),levels = 0:1,labels = c("non-Disabled","Disabled"))
+fig_dsistribution(dataFrq)
+
+
+
+rm(list= ls()[!(ls() %in% c(Keep.List))])
+res <- readRDS("results/estimations/CropID_Pooled_disabled_TL_hnormal_optimal.rds")$disagscors
+res$disasg <- res$disagscors_var
+res$level <- res$disagscors_level
+res <- res[res$estType %in% "teBC",]
+res <- res[res$Survey %in% "GLSS0",]
+res <- res[res$restrict %in% "Restricted",]
+res <- res[res$stat %in% "mean",]
+res <- res[!res$sample %in% "unmatched",]
+res <- res[res$CoefName %in% "disag_efficiencyGap_pct",]
+res <- res[res$CoefName %in% "disag_efficiencyGap_pct",]
+res <- res[res$input %in% "MTE",]
+
+reg <- res[res$disagscors_var %in% "Region",]
+reg <- reg[order(reg$Estimate),]
+paste0(paste0(reg$level," (",round(reg$Estimate,2),"%)"),collapse = ", ")
+
+CROP <- res[res$disagscors_var %in% "CROP",]
+CROP <- CROP[order(CROP$Estimate),]
+paste0(paste0(CROP$level," (",round(CROP$Estimate,2),"%)"),collapse = ", ")
+
+
+
+
+
+
+
 
 
 
